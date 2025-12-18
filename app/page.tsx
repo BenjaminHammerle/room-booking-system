a71c60593347f7ac54db72c9c035ada4bd8f90ea
Benjamin Hammerle <hb6912@mci4me.at>
2025-12-18 23:20:09 +0100

    Add Anleitungen.txt

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,18 @@
-export default function Home() {
-  return (
-      <main className="p-8">
-        <h1 className="text-3xl font-bold">
-          Room Booking System
-        </h1>
-        <p className="mt-4">
-          Studienprojekt – Next.js
-        </p>
-      </main>
-  );
-}
+export const metadata = {
+    title: 'Room-Booking-System | Startseite', // Das ist der Titel im Browser-Tab
+};
 
+
+export default function Home() {
+    return (
+        <main className="p-8">
+            <h1 className="text-3xl font-bold">
+                Room Booking System
+            </h1>
+
+            <p className="mt-4 text-gray-600">
+                Studienprojekt mit Next.js
+            </p>
+        </main>
+    );
+}
